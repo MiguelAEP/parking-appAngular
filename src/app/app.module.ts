59581aa886e40d06cd011e99d2f549ddc0069215
miguelAEP <miguelep24@hotmail.com>
2024-08-15 17:17:07 -0500

    comenzando el proyecto estacionamiento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './Components/headerComponent/header/header.component';
+import { AsideComponent } from './Components/asideComponent/aside/aside.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AsideComponent
+    
   ],
   imports: [
     BrowserModule,
